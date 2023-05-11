9b7c4b4763f96b06463a5a1f154a85af1d3ce571
Amin Siddiqui <staticVoidMan@users.noreply.github.com>
2023-05-11 21:09:27 +0530

    #02_02.1

---
+++ Ex_Files/02_02/main.py
@@ -0,0 +1,13 @@
+greet = "Hello, World!"
+print(greet)
+
+combined = "Hello, World!" + " Lets Go!"
+print(combined)
+
+name = "Python Jr."
+formattedLiteral = f"Hello, {name}!"
+print(formattedLiteral)
+
+name = "Python Sr."
+placeholderFormattedLiteral = "Hello, {}!"
+print(placeholderFormattedLiteral.format(name))
